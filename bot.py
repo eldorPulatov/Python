687c03225495ceffad80a1f30fb2ccd183cdd801
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2023-07-29 19:17:52 +0300

    function for telegram-bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,40 @@
-from aiogram import Bot, Dispatcher, executor, types
+from bot_func_abc import BotFunctionABC
+import telebot
+from telebot import types
+from telebot.callback_data import CallbackData
+from typing import List
+import requests
 
-# t.me/botForExam_bot
-API_TOKEN = '5995846876:AAGJz3ioOTZw6aK9gaxg88gXBN9c6LizPKo'
+class NYTimes_science(BotFunctionABC):
+    def set_handlers(self, bot: telebot.TeleBot, commands: List[str]):
+        self.bot = bot
+        api_key = "JOv1U7L6ISqj8jAWPIS3SUHAv33sNfEn"
+        self.url = f"https://api.nytimes.com/svc/topstories/v2/science.json?api-key={api_key}"
+        self.example_keyboard_factory = CallbackData('t_key_button', prefix=commands[0])
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+        @bot.message_handler(commands=commands)
+        def check_status(message: types.Message):
+            response = self.Top_stories()
+            bot.send_message(message.chat.id, response)
 
-
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    def Top_stories(self):
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                data = response.json()
+                
+                # Получение списка новостей
+                articles = data['results']
+                headlines = ""
+                
+                # Перебор новостей и добавление заголовков в переменную
+                for article in articles:
+                    title = article['title']
+                    headlines += title + '\n'
+                    
+                # Заголовки новостей в области науки NYTiems
+                return headlines
+            else:
+                return "Произошла ошибка при проверке состояния серверов NYTimes"
+        except requests.exceptions.RequestException as e:
+            return f"Ошибка при выполнении запроса: {str(e)}"
